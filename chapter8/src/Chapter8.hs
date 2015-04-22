9a5b19b762164f7a2cb619a5649bd5756d9f4a9b
danielle kefford <danielle_kefford@hotmail.com>
2015-04-21 23:19:03 -0400

    Finished two more exercises in chapter 8.

--- chapter8/src/Chapter8.hs
+++ chapter8/src/Chapter8.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Chapter8 (globToRegex, matchesGlob) where
 
+import Control.Exception (SomeException, handle)
+import Control.Monad (forM)
 import Data.Char (isUpper, toLower, toUpper)
+import System.Directory (doesDirectoryExist, doesFileExist,
+                         getCurrentDirectory, getDirectoryContents)
+import System.FilePath ((</>), dropTrailingPathSeparator, splitFileName)
+import System.Info (os)
+import System.Posix.Files (fileExist)
 import Text.Regex.Posix
+
+-- First set of exercises.
+
+-- 2.
 
 -- | User-facing function to convert glob to regex pattern,
 -- with case-sensitivity turned on or off depending in the input flag.
@@ -10,8 +22,8 @@ globToRegex caseFlag glob = '^' : globToRegex' caseFlag glob ++ "$"
 
 -- | User-facing function to match files according to the input glob
 -- with case-sensitivity turned on or off depending in the input flag.
-matchesGlob :: FilePath -> Bool-> String -> Bool
-matchesGlob name caseFlag glob = name =~ globToRegex caseFlag glob
+matchesGlob :: Bool-> String -> FilePath -> Bool
+matchesGlob caseFlag glob name = name =~ globToRegex caseFlag glob
 
 -- | Helper function for glob to regex conversion.
 globToRegex' :: Bool-> String -> String
@@ -46,3 +58,69 @@ flipCase :: Char -> Char
 flipCase c
   | isUpper c = toLower c
   | otherwise = toUpper c
+
+-- Second set of exercises.
+
+isPattern :: String -> Bool
+isPattern = any (`elem` "[*?")
+
+isHidden :: String -> Bool
+isHidden ('.':_) = True
+isHidden _       = False
+
+-- 1. listMatches modified to use new version of matchesGlob based on host OS.
+--
+-- N.B. Looks like something changed in handle since the book was
+-- published and the type of Exception needs to be specified or
+-- you'll get the following error upon compilation:
+--
+--   No instance for (GHC.Exception.Exception e0)
+--     arising from a use of ‘handle’
+--
+-- I relied on this SO answer:
+--
+--   http://stackoverflow.com/questions/3670098/ghc-compilation-error-arising-from-import-of-control-exception#3670366
+listMatches :: FilePath -> String -> IO [String]
+listMatches dirName pat = do
+    dirName' <- if null dirName
+                then getCurrentDirectory
+                else return dirName
+    handle (\(_ :: SomeException) -> return []) $ do
+        names <- getDirectoryContents dirName'
+        let caseFlag = if os =~ "mingw"
+                       then True
+                       else False
+            names' = if isHidden pat
+                     then filter isHidden names
+                     else filter (not . isHidden) names
+        return (filter (matchesGlob caseFlag pat) names')
+
+-- 2. Both namesMatching and listPlain modified to use fileExist.
+
+namesMatching pat
+  | not (isPattern pat) = do
+    exists <- fileExist pat
+    return (if exists then [pat] else [])
+  | otherwise = do
+    case splitFileName pat of
+      ("", baseName) -> do
+          curDir <- getCurrentDirectory
+          listMatches curDir baseName
+      (dirName, baseName) -> do
+          dirs <- if isPattern dirName
+                  then namesMatching (dropTrailingPathSeparator dirName)
+                  else return [dirName]
+          let listDir = if isPattern baseName
+                        then listMatches
+                        else listPlain
+          pathNames <- forM dirs $ \dir -> do
+                           baseNames <- listDir dir baseName
+                           return (map (dir </>) baseNames)
+          return (concat pathNames)
+
+listPlain :: FilePath -> String -> IO [String]
+listPlain dirName baseName = do
+    exists <- if null baseName
+              then doesDirectoryExist dirName
+              else fileExist (dirName </> baseName)
+    return (if exists then [baseName] else [])
